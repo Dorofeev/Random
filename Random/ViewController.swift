bc30008bc9d43451b8f7d0ba9297f4c4d8a33762
adorofeev <adorofeev@icerockdev.com>
2021-08-26 15:26:38 +0700

    Initial Commit

---
+++ Random/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random
+//
+//  Created by Andrey Dorofeev on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
