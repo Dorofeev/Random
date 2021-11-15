9869865481ffadb0cba1e21070806ef31fa11828
adorofeev <adorofeev@icerockdev.com>
2021-11-15 10:20:51 +0700

    added stacks for min max values

--- Random/Features/RandomNumber/Controller/RandomNumberView.swift
+++ Random/Features/RandomNumber/Controller/RandomNumberView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct RandomNumberView: View {
+    @State private var min: String = "0"
+    @State private var max: String = "100"
+    
     var body: some View {
         VStack(spacing: 36.0) {
             Text("Current Random Number")
@@ -20,14 +23,26 @@ struct RandomNumberView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
             Spacer()
-            Text("Min:")
-                .font(.title)
-                .fontWeight(.medium)
-                .foregroundColor(Color.black)
-            Text("Max:")
-                .font(.title)
-                .fontWeight(.medium)
-                .foregroundColor(Color.black)
+            HStack {
+                Text("Min:")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.black)
+                TextField("Minimum number ", value: $min, formatter: NumberFormatter())
+                    .keyboardType(.numberPad)
+            }
+            
+            HStack {
+                Text("Max:")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.black)
+                TextField("Maximum number ", value: $max, formatter: NumberFormatter())
+                    .keyboardType(.numberPad)
+            }
+            Button("Generate") {
+                print("generate")
+            }
             Spacer()
         }
     }
